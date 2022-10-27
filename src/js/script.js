8a294eb2e04d1f36affcb7d461b8c7a6e3e0e461
Michał Słota <michal.k.slota@gmail.com>
2022-10-27 14:56:28 +0200

    add favorite books function

--- src/js/script.js
+++ src/js/script.js
@@ -15,8 +15,8 @@
 
     const templates = {
         book: Handlebars.compile(document.querySelector(select.templateOf.book).innerHTML),
-    }
-
+    };
+    
     function render (){
         for (let book of dataSource.books){
             //console.log(book);
@@ -28,7 +28,29 @@
             //console.log('wrapper', booksContainer);
             booksContainer.appendChild(generatedDOM);
         }
+    };
+
+    const favoriteBooks = [];
+    function initActions(){
+    
+        const bookImages = document.querySelectorAll('.book__image')
+        console.log(bookImages);
+        for (const image of bookImages){
+            image.addEventListener('dblclick', function(event){
+                event.preventDefault();
+                const imageId = image.getAttribute('data-id');
+                if (!favoriteBooks.includes(imageId)){
+                image.classList.add("favorite");
+                favoriteBooks.push(imageId);
+                }else{
+                    image.classList.remove("favorite");
+                    favoriteBooks.splice(favoriteBooks.indexOf(imageId));
+                }
+                console.log(favoriteBooks);
+            });
+        }
     }
 
     render();
+    initActions();
 }
